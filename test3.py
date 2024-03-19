e4bdfcff94651edc95f1bfbd55e19222ceb6a1f7
hjs011 <jshwang0118@gmail.com>
2024-03-19 12:16:03 +0900

    fixed version 5

--- test3.py
+++ test3.py
@@ -1,8 +1,7 @@
 # This is the third project.
+# Let's fix this code.
 for i in range(401, 501):
     if i % 5 == 0:
-        pass
-    else:
-        print(i)
+        print('5의 배수입니다')
 
 # Project ends.
