b0f8728bad4e6c83d821679b33f6a5a445a85a0c
hjs011 <jshwang0118@gmail.com>
2024-03-19 11:49:10 +0900

    fixed version 1

--- test.py
+++ test.py
@@ -1,23 +1,13 @@
 # git_flow test
 for i in range(1, 101):
     if i % 2 == 0:
-<<<<<<< HEAD
-=======
         pass
     else:
         print(i)
 
 # start a new project
-for i in range(101, 201):
-    if i % 2 == 0:
->>>>>>> ff4e7a3b38ffbcd77c9933d1869a40c9b7c8aac3
-        pass
-    else:
-        print(i)
+for j in range(101, 201):
+    print(j)
 
-# start a new project
-for i in range(201, 301):
-    if i % 2 == 0:
-        pass
-    else:
-        print(i)
+for k in range(201, 301):
+    print(k)
