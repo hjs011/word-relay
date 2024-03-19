b8ce3a56780b17e0cd25458b12a9477e052292c2
hjs011 <jshwang0118@gmail.com>
2024-03-19 11:43:18 +0900

    first commit

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 # git_flow test
 for i in range(1, 101):
-    if i & 2 == 0:
+    if i % 2 == 0:
+        pass
+    else:
+        print(i)
+
+# start a new project
+for i in range(201, 301):
+    if i % 2 == 0:
         pass
     else:
         print(i)
